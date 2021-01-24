5803a378725496b9ed9268287cdee737ac685d84
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-01-24 22:45:01 +0100

    indentation correction

--- src/GildedRose.py
+++ src/GildedRose.py
@@ -56,6 +56,7 @@ class AgedBrie(NormalItem):
         Item.__init__(self, name, sell_in, quality)
 
     def update_quality(self):
+
         pass
 
 
@@ -76,36 +77,3 @@ class Backstage(NormalItem):
         return self.quality
 
 ####REFACTORIZABLE BRO####
-
-
-class GildedRose(object):
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-
-class Updatable:
-    def update_quality(self):
-        pass
-
-
-class NormalItem(Item, Updatable):
-
-
-class ConjuredItem(Item, Updatable):
-
-
-class Sulfuras(NormalItem):
-
-
-class AgedBrie(NormalItem):
-
-
-class Backstage(NormalItem)
