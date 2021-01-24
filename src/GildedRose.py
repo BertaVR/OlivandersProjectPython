7564762b3c374c796e49e7de23e56002f1bf33af
ToniCaimari <acaimari@cifpfbmoll.eu>
2021-01-24 22:40:26 +0100

    Backstage class_update_quality function pasedtests

--- src/GildedRose.py
+++ src/GildedRose.py
@@ -1,3 +1,83 @@
+class Inventory(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            item.update_quality()
+
+
+class Item:
+    def __init__(self, name, quality, sell_in):
+        self.name = name
+        self.quality = quality
+        self.sell_in = sell_in
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.quality, self.sell_in)
+
+    # holaholahola esto lo estoy escribiendo desde rama Berta
+    # jjbujbub
+
+
+class Updateable:
+    # def ItemTypes():
+    #     AgedItems=['AgedBrie']
+    #     EventItems=['Backstage']
+    #     Legendaries=['Sulfuras']
+    #     Conjured=[]
+
+    def update_quality(self):
+        pass
+
+
+class ConjuredItem(Item, Updateable):
+    def __init__(self, name, quality, sell_in):
+        Item.__init__(self, name, sell_in, quality)
+
+
+class NormalItem(Item, Updateable):
+    def __init__(self, name, quality, sell_in):
+        Item.__init__(self, name, sell_in, quality)
+
+
+class Sulfuras(NormalItem):
+    def __init__(self, name, quality, sell_in):
+        Item.__init__(self, name, sell_in, quality)
+
+    def update_quality(self):
+
+        pass
+
+
+class AgedBrie(NormalItem):
+    def __init__(self, name, quality, sell_in):
+        Item.__init__(self, name, sell_in, quality)
+
+    def update_quality(self):
+        pass
+
+
+class Backstage(NormalItem):
+    def __init__(self, name, quality, sell_in):
+        Item.__init__(self, name, quality, sell_in)
+
+    def update_quality(self):
+
+        if self.sell_in > 10:
+            self.quality += 1
+        elif self.sell_in > 5:
+            self.quality += 2
+        elif self.sell_in > 0:
+            self.quality += 3
+        elif self.sell_in == 0:
+            self.quality = 0
+        return self.quality
+
+####REFACTORIZABLE BRO####
+
+
 class GildedRose(object):
 
 
--- test/test_GildedRose.py
+++ test/test_GildedRose.py
@@ -1,5 +1,6 @@
-# inventario
+from src.GildedRose import *
 
+# inventario
 # sulfuras
 
 # backstage
@@ -8,3 +9,19 @@
 
 
 # conjured
+# jamon, pata, escoba, libroencantado - -- libroencantad
+
+"""
+def ItemTypes():
+    AgedItems = ['AgedBrie']
+    EventItems = ['Backstage']
+    Legendaries = ['Sulfuras']
+    Conjured = []
+    """
+
+
+def test_Backstage():
+    assert Backstage("backstage_passes", 15, 0).update_quality() == 0
+    assert Backstage("backstage_passes", 15, 4).update_quality() == 18
+    assert Backstage("backstage_passes", 15, 6).update_quality() == 17
+    assert Backstage("backstage_passes", 0, 11).update_quality() == 1
